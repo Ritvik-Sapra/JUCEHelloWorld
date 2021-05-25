da6b455937a143d3021f6dcd82a2210333cc5bb3
Ritvik Sapra <sapra.ritvik@outlook.com>
2021-05-26 00:14:38 +0530

    Changed Font size

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,8 +16,8 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (16.0f));
-    g.setColour (juce::Colours::white);
+    g.setFont (juce::Font (32.0f));
+    g.setColour (juce::Colours::whitesmoke);
     g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
